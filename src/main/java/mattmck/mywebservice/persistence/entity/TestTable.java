f03b7da80fc4d07aa6a6883d3a7353d4300e7b72
Matt Mckenna <mattmck@gmail.com>
2024-08-26 18:54:31 +0800

    initial commit

--- src/main/java/mattmck/mywebservice/persistence/entity/TestTable.java
+++ src/main/java/mattmck/mywebservice/persistence/entity/TestTable.java
@@ -1,22 +1,22 @@
-package mattmck.mywebservice.persistence.entity;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import lombok.Data;
-
-@Entity
-@Table(name="PGDB_XA_TEST")
-@Data
-public class TestTable {
-	
-	@Id
-	//@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="ID")
-	private String id;
-	
-	@Column(name="VALUE")
-	private String value;
-}
+//package mattmck.mywebservice.persistence.entity;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//import lombok.Data;
+//
+//@Entity
+//@Table(name="PGDB_XA_TEST")
+//@Data
+//public class TestTable {
+//	
+//	@Id
+//	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@Column(name="ID")
+//	private String id;
+//	
+//	@Column(name="VALUE")
+//	private String value;
+//}
